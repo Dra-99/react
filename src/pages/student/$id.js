6a98db6bd0f0f3195916214fb20969b296655c7c
Dra-99 <wbl111@aliyun.com>
2021-12-08 21:06:18 +0800

    umi-学生管理

--- src/pages/student/$id.js
+++ src/pages/student/$id.js
@@ -5,10 +5,11 @@
  */
 import React from 'react'
 
-export default function $id() {
+export default function $id(props) {
     return (
         <div>
             <h1>修改学生</h1>
+            <h1>{props.match.params.id}</h1>
         </div>
     )
 }
